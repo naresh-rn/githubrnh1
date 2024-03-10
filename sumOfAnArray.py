afa1f59ce6f1d199296285d5151bae17d941d627
naresh-rn <naresh.ravic@gmail.com>
2024-03-10 09:39:57 -0700

    files added

---
+++ sumOfAnArray.py
@@ -0,0 +1,10 @@
+from sumOfAnArray import *
+
+def sumOfArray(array_list):                         # function definition
+    sum = 0
+    for item in range(0, len(array_list)):
+        sum = sum + array_list[item]
+    return sum
+# function calling
+array_list = array('i', [54,47,68,84,927,10])
+print("Sum of array is :",sumOfArray(array_list))
