18c0f542554926bb60578556b4707035b9cb80b6
naresh-rn <naresh.ravic@gmail.com>
2024-03-10 05:39:23 -0700

    sumOfAllElements in array

---
+++ binarySearch.py
@@ -0,0 +1,3 @@
+# from array import *
+# array_list = array('i', [1,274,49,63,783,19202,278])
+# print(array_list)
---
+++ factorialOfNumber.py
@@ -0,0 +1,16 @@
+# function definition
+# def factorial(number):
+#     fact = 1
+#     for index in range(1, number + 1):
+#         fact = fact * index
+#     return fact
+#
+#
+# # function calling
+# factorialValue = factorial(5)
+# print(factorialValue)
+
+
+
+# help(list)
+
