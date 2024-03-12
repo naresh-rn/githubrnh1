1708687bb5db34dd3844436b9dbc8c110ba6f838
Naresh R <149216548+naresh-rn@users.noreply.github.com>
2024-03-12 09:38:40 -0700

    Delete linearSearch.py

--- linearSearch.py
+++
@@ -1,9 +0,0 @@
-from sumOfAnArray import *
-array = array('i',[1,2,4,56,788,20])
-
-# use exception
-findElement = int(input("Enter a number : "))
-for index in range(1, len(array)):
-    if array[index] == findElement:
-        print("Element index is : ",index)
-
